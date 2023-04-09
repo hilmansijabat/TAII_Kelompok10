27190fd7f80e181c4b9cf79a11dcc8752153a0e1
Andree Panjaitan <andreepanjaitan@Andrees-MacBook-Pro.local>
2023-04-09 15:19:34 +0700

    try solve static file problem

--- app/app/settings.py
+++ app/app/settings.py
@@ -109,15 +109,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_ROOT = "/vol/static"
 
+STATIC_ROOT = "/vol/static"
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
